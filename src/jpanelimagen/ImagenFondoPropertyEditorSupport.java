10ae77fb6aa1c455c595737c904df14cbe7cdd6a
Dani <Dani@Dani>
2023-10-27 18:46:22 +0200

    Cuarto vídeo, Editamos el panel personalizable creado anteriormente y lo enlazamos para que pueda recoger los valores introducidos y pintar la imagen.

---
+++ src/jpanelimagen/ImagenFondoPropertyEditorSupport.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jpanelimagen;
+
+import java.awt.Component;
+import java.beans.PropertyEditorSupport;
+
+/**
+ *
+ * @author Dani
+ */
+public class ImagenFondoPropertyEditorSupport extends PropertyEditorSupport 
+{
+    private ImagenFondoPanel imagenFondoPanel = new ImagenFondoPanel();
+
+    @Override
+    public boolean supportsCustomEditor() {
+        return true;
+    }
+
+    @Override
+    public Component getCustomEditor() {
+        return imagenFondoPanel;
+    }
+
+    @Override
+    public String getJavaInitializationString() {
+        return super.getJavaInitializationString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public Object getValue() {
+        return imagenFondoPanel.getSelectedValue(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
